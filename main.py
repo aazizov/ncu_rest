3e3b72e8a10ea3a2ec72cbc6baddd45b1d25198e
“Alakbar <a.azizov@yahoo.com>
2025-06-30 11:55:23 +0400

    First Get requests

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+#1
+@app.get("/getstate/{address}")
+async def getstate(address: str):
+    return {"message": f"GetState for {address}"}
 
+#3
+@app.get("/getallstates")
+async def getallstates():
+    return {"message": "States ..."}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+#5.1
+@app.get("/querytime/{address}")
+async def querytime(address: str):
+    return {"message": f"QueryTime for {address}"}
+
+#8.1
+@app.get("/queryaddresstate/{address}")
+async def queryaddresstate(address: str):
+    return {"message": f"QueryAddrState for {address}"}
+
+#8.2
+@app.get("/querybusstate/{bus}")
+async def querytime(bus: str):
+    return {"message": f"QueryBusState for {bus}"}
