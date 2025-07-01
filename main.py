3c093997acf697103461c21a5553bcafa8957379
alakbar <a.azizov@yahoo.com>
2025-07-01 06:12:32 +0400

    Some functions ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-# from kafka import KafkaProducer
+from kafka import KafkaProducer
 import json
 import serial
 import time
@@ -28,18 +28,18 @@ ncu_settings = {
 }
 
 ncu_commands = {
-    "getstate_": "02",
-    "_getstate": "300335",
-    "unlock_": "02",
-    "_unlock": "310338",
-    "getallstates": "02F0320327",
-    "openall_": "02",
-    "_openall": "330338",
-    "querytime": "020037033C",
-    "queryaddressstate_": "02",
-    "_queryaddressstate": "3A033F",
-    "querybusstate_": "02",
-    "_querybusstate": "3A032F"
+    "getstate_": "02",              # 1
+    "_getstate": "300335",          # 1
+    "unlock_": "02",                # 2
+    "_unlock": "310338",            # 2
+    "getallstates": "02F0320327",   # 3
+    "openall_": "02",               # 4
+    "_openall": "330338",           # 4
+    "querytime": "020037033C",      # 5.1
+    "queryaddressstate_": "02",     # 8.1
+    "_queryaddressstate": "3A033F", # 8.1
+    "querybusstate_": "02",         # 8.2
+    "_querybusstate": "3A032F"      # 8.2
 }
 
 ser = serial.Serial(
@@ -56,49 +56,153 @@ ser = serial.Serial(
 async def getstate(address: str):
     if (ncu_settings["request_hex"]):
         command = ncu_commands["getstate_"] + address + ncu_commands["_getstate"]
-#    return {"message": f"GetState {command} for {address}"}
-#    return {"response": f"{command}"}
-    if (ncu_settings["response_hex"]):
-        response = f"{command}"
-    else:
-        response = f"All OK"
+        bytes_to_send = bytes.fromhex(command)
+        # Send data to the serial device
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
 
-    if(ncu_settings["kafka_message_long"]):
-        kafka_message = f"/getstate/{address}" + "|" + response
-    print(kafka_message)
-    return response
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+        # Read data from the serial device
+        # Read all bytes waiting in the input buffer
+        if ser.in_waiting > 0:
+            received_data = ser.read(ser.in_waiting)
+            print(f"Received: {received_data}")
+        else:
+            print("No data received.")
+    return {"response": f"{received_data}"}
 
 #2
 @app.put("/unlock/{address}")
 async def unlock(address: str):
-    return {"response": f"Unlock for {address}"}
+    if (ncu_settings["request_hex"]):
+        command = ncu_commands["unlock_"] + address + ncu_commands["_unlock"]
+        bytes_to_send = bytes.fromhex(command)
+        # Send data to the serial device
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
+
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+    return {"response": "None"}
 
 #3
 @app.get("/getallstates")
 async def getallstates():
-    return {"response": "States..."}
+    if (ncu_settings["request_hex"]):
+        command = ncu_commands["getallstates"]
+        bytes_to_send = bytes.fromhex(command)
+        # Send data to the serial device
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
+
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+        # Read data from the serial device
+        # Read all bytes waiting in the input buffer
+        if ser.in_waiting > 0:
+            received_data = ser.read(ser.in_waiting)
+#            print(f"Received: {received_data.decode().strip()}")
+            print(f"Received: {received_data}")
+        else:
+            print("No data received.")
+
+    return {"response": {received_data}}
 
 #4
-@app.put("/openall/{bus}")
+@app.put("/openall/{bus}")      # Default: '0200330338'
 async def openall(bus: str):
-    command = ncu_commands["getstate_"] + bus + ncu_commands["_getstate"]
-    bytes_to_send = bytes.fromhex(command)
+    if (ncu_settings["request_hex"]):
+        command = ncu_commands["openall_"] + bus + ncu_commands["_openall"]
+        bytes_to_send = bytes.fromhex(command)
     # Send data to the serial device
-    ser.write(bytes_to_send)
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
 
-    return {"response": f"Openall... for {bus}"}
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+        # Read data from the serial device
+        # Read all bytes waiting in the input buffer
+        if ser.in_waiting > 0:
+            received_data = ser.read(ser.in_waiting)
+#            print(f"Received: {received_data.decode().strip()}")
+            print(f"Received: {received_data}")
+        else:
+            print("No data received.")
+#    return {"response": f"Openall... for {bus}"}
+    return {"response": f"{received_data}"}
 
 #5.1
 @app.get("/querytime")
 async def querytime():
-    return {"response": f"QueryTime... "}
+    if (ncu_settings["request_hex"]):
+        command = ncu_commands["querytime"]
+        bytes_to_send = bytes.fromhex(command)
+        # Send data to the serial device
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
+
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+        # Read data from the serial device
+        # Read all bytes waiting in the input buffer
+        if ser.in_waiting > 0:
+            received_data = ser.read(ser.in_waiting)
+            print(f"Received: {received_data}")
+        else:
+            print("No data received.")
+
+    return {"response": {received_data}}
+
 
 #8.1
 @app.get("/queryaddresstate/{address}")
 async def queryaddresstate(address: str):
-    return {"response": f"QueryAddrState for {address}"}
+    if (ncu_settings["request_hex"]):
+        command = ncu_commands["queryaddressstate_"] + address + ncu_commands["_queryaddressstate"]
+        bytes_to_send = bytes.fromhex(command)
+        # Send data to the serial device
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
+
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+        # Read data from the serial device
+        # Read all bytes waiting in the input buffer
+        if ser.in_waiting > 0:
+            received_data = ser.read(ser.in_waiting)
+            print(f"Received: {received_data}")
+        else:
+            print("No data received.")
+            return {"response": "None"}
+    return {"response": f"{received_data}"}
+
 
 #8.2
 @app.get("/querybusstate/{bus}")
 async def querytime(bus: str):
-    return {"response": f"QueryBusState for {bus}"}
+    if (ncu_settings["request_hex"]):
+        command = ncu_commands["querybusstate_"] + bus + ncu_commands["_querybusstate"]
+        bytes_to_send = bytes.fromhex(command)
+        # Send data to the serial device
+        ser.write(bytes_to_send)
+        print(f"Sending: {bytes_to_send.hex()}")
+
+        # Wait a moment for the device to respond
+        time.sleep(0.1)
+
+        # Read data from the serial device
+        # Read all bytes waiting in the input buffer
+        if ser.in_waiting > 0:
+            received_data = ser.read(ser.in_waiting)
+            print(f"Received: {received_data}")
+        else:
+            print("No data received.")
+            return {"response": "None"}
+    return {"response": f"{received_data}"}
