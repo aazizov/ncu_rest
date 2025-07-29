ae0485b0bfd8500be0a3167569fbc784abe74735
alakbar <a.azizov@yahoo.com>
2025-07-29 10:28:01 +0400

    Added Try-Delay of Connection for COM port from .env file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
-from datetime import datetime
+import threading
 
-from cleo.io.null_io import NullIO
+from datetime import datetime
+from warnings import catch_warnings
+
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -31,57 +33,49 @@ class Response_Json(BaseModel):
     lock: str = 'Open'
     infrared: str = '01'
 
+class GetVersion_Json(BaseModel):
+    addr_board_dec: int = 1     # Board#1
+#    addr_board_hex: str = ''
+
 class GetState_Json(BaseModel):
-    addr_dec: int = '1'
-    addr_hex: str = ''
+    board: int = 1
+#    lock: int = 1
+#    addr_dec: int = '1'
 
 class Unlock_Json(BaseModel):
-    addr_ncu_dec: int = 1
-    addr_ncu_hex: str = ''
-    addr_lock_dec: int = 1
-    addr_lock_hex: str = ''
+    board: int = 1
+    lock: int = 1
 
-class Openall_Json(BaseModel):
-    addr_ncu_dec: int = 1
-    addr_ncu_hex: str = ''
+class UnlockAll_Json(BaseModel):
+    board: int = 1
 
 class Getunlocktime_Json(BaseModel):
-    addr_ncu_dec: int = 1
+    board: int = 1
     addr_ncu_hex: str = ''
 
 class Setunlocktime_Json(BaseModel):
-    addr_ncu_dec: int = 1
+    board: int = 1
     addr_ncu_hex: str = ''
     set_unlocktime_ms_dec: int = 550     # ==550ms   # ==0x27*10=550 ms
     set_unlocktime_ms_hex: str = ''
 
-app = FastAPI()
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-kafka_address = os.getenv("KAFKA_ADDRESS")
-kafka_port = os.getenv("KAFKA_PORT")
+kafka_address = str(os.getenv("KAFKA_IP"))
+kafka_port = str(os.getenv("KAFKA_PORT"))
 kafka_topic = os.getenv("KAFKA_TOPIC")
+rs485_address = os.getenv("COM_PORT")
+max_retries = int(os.getenv("MAX_RETRIES"))
+retry_delay_seconds = int(os.getenv("RETRY_DELAY_SECONDS"))
 
-'''
+
 producer = KafkaProducer(
     bootstrap_servers= kafka_address+':'+kafka_port, #'localhost:9092',
     api_version=(0,11,5),
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
-'''
 
-rs485_address = '/dev/cu.usbserial-B000KAZT' #   port= 'COM3',  # PC RS232 (USB-CAT5)   # Serial port name
-
-'''
-class Response(BaseModel):
-    date_time1 : datetime.now()
-    rest_command : str
-    date_time2 : datetime.now()
-    rest_command : in_485
-    date_time3 : datetime.now()
-    rest_command : in_485
-'''
 
 ncu_settings = {
     "request_hex": True,        # HEX for address, bus
@@ -115,77 +109,70 @@ ncu_commands = {
     "_querybusstate": "3A032F"      # 8.2
 }
 
-# Commented Temporary
-ser = serial.Serial(
-    port= rs485_address, # '/dev/cu.usbserial-B000KAZT', # MacBook RS232 'COM1',     # Serial port name
-    baudrate=19200,          # Baud rate
-    parity=serial.PARITY_NONE, # Parity setting (e.g., serial.PARITY_ODD, serial.PARITY_EVEN)
-    stopbits=serial.STOPBITS_ONE, # Stop bits (e.g., serial.STOPBITS_TWO)
-    bytesize=serial.EIGHTBITS,  # Data bits (e.g., serial.SEVENBITS)
-    timeout=1               # Read timeout in seconds
-)
 
-def check_sum(request_json: Request_Json):
-    return hex(int(('0x' + request_json.stx), 16)
-                + int(('0x' + request_json.addr), 16)
-                + int(('0x' + request_json.cmd), 16)
-                #                     + int(('0x'+data),16)
-                + int(('0x' + request_json.etx), 16))[-2:]  # last 1(one)[2 symbols] byte from summa
+for attempt in range(int(max_retries)):
+    try:
+        ser = serial.Serial(
+            port=rs485_address,  # '/dev/cu.usbserial-B000KAZT', # MacBook RS232 'COM1',     # Serial port name
+            baudrate=19200,  # Baud rate
+            parity=serial.PARITY_NONE,  # Parity setting (e.g., serial.PARITY_ODD, serial.PARITY_EVEN)
+            stopbits=serial.STOPBITS_ONE,  # Stop bits (e.g., serial.STOPBITS_TWO)
+            bytesize=serial.EIGHTBITS,  # Data bits (e.g., serial.SEVENBITS)
+            timeout=1  # Read timeout in seconds
+        )
+        print(ser)
+        break  # Exit loop if operation succeeds
+    except serial.SerialException as e:
+        print(f"Attempt {attempt + 1} failed: {e}")
+        if attempt < max_retries - 1:
+            print(f"Retrying in {retry_delay_seconds} seconds...")
+            time.sleep(retry_delay_seconds)
+        else:
+            print("Maximum retries reached. Operation failed permanently.")
+            try:
+                producer.send(kafka_topic, f"Attempt at {datetime.now()} to Connect to {rs485_address} failed")
+            except Exception as e:
+                print(f"Failed to send message to {kafka_address+':'+kafka_port} topic {kafka_topic}; {e}")
 
-def check_sum2(address: str, command: str, data: str):
-    return hex(int(('0x' + stx), 16)
-                + int(('0x' + address), 16)
-                + int(('0x' + command), 16)
-                #                     + int(('0x'+data),16)
-                + int(('0x' + etx), 16))[-2:]  # last 1(one)[2 symbols] byte from summa
+'''
+# Start of Program Initialization
+try:
+# To Find and Connect to Serial Port
+    ser = serial.Serial(
+        port= rs485_address, # '/dev/cu.usbserial-B000KAZT', # MacBook RS232 'COM1',     # Serial port name
+        baudrate=19200,          # Baud rate
+        parity=serial.PARITY_NONE, # Parity setting (e.g., serial.PARITY_ODD, serial.PARITY_EVEN)
+        stopbits=serial.STOPBITS_ONE, # Stop bits (e.g., serial.STOPBITS_TWO)
+        bytesize=serial.EIGHTBITS,  # Data bits (e.g., serial.SEVENBITS)
+        timeout=1               # Read timeout in seconds
+    )
+except serial.SerialException as e:
+    print(f"Cannot open COM Port. {e}")
+'''
 
-def check_sum3(address: str, command: str, data: str):
-    return hex(int(('0x' + stx), 16)
-                + int(('0x' + address), 16)
-                + int(('0x' + command), 16)
-                + int(('0x' + data),16)
-                + int(('0x' + etx), 16))[-2:]  # last 1(one)[2 symbols] byte from summa
+
+app = FastAPI()
 
 
-def serial_txrx(send: str):
-    # Send data to the serial device
-    ser.write(bytes.fromhex(send))
-    # Wait a moment for the device to respond
-    time.sleep(0.1)
-    # Read data from the serial device
-    # Read all bytes waiting in the input buffer
-    if ser.in_waiting > 0:
-        # We have to exclude symbol ';' from the end of NCU16 response.
-        received_data = ser.read(ser.in_waiting).replace(b';', b'')
-        print(f"Received: {received_data}")
-    else:
-        received_data = bytes("None Response", 'utf-8')
-        print("No data received.")
-    return {'response_time': datetime.now(),
-            'response_bytes': received_data,
-            'response_hex': received_data.hex() #,
-            #'response_bin': bin(int(received_data, 16))[2:].zfill(len(received_data) * 4)
-            }
 
 #1
 #@app.get("/getstate/{address}")    # By URL
 @app.put("/getstate/")     # By Json
 async def getstate(getstate_json: GetState_Json):
+    '''
+    We have to send number of Board (1-10).
+    '''
     time_received = datetime.now()
-    if getstate_json.addr_dec != '':
-        getstate_json.addr_dec = getstate_json.addr_dec - 1
-#        addr = getstate_json.addr_dec
-        addr = str(getstate_json.addr_dec.to_bytes(1, byteorder='little'))[4:6]
-    else:
-        addr = getstate_json.addr_hex
+    getstate_json.board = getstate_json.board - 1
+    addr_ncu = str(getstate_json.board.to_bytes(2, byteorder='little'))[5:6]
+
+    addr = addr_ncu + '0'
 
     cmd = '30'
     summa = check_sum2(addr, cmd, '')
     command = stx + addr + cmd + etx + summa
     time_sent = datetime.now()
     response = serial_txrx(command)
-#    print(response['response_hex'][6:10])
-#    result = response['response_hex'][6:10]
     result = response['response_hex'][8:10] + response['response_hex'][6:8] # reverse bytes = 0900 -> 0009
     result_bin = bin(int(result))
 
@@ -208,23 +195,17 @@ async def getstate(getstate_json: GetState_Json):
             },
     }
 
-#2
-#@app.put("/unlock/{address}")
 @app.put("/unlock/")
-#async def unlock(address: str):
 async def unlock(unlock_json: Unlock_Json):
+    '''
+    We have to send number of Board (1-10), and number of Lock(1-16).
+    '''
     time_received = datetime.now()
-    if unlock_json.addr_ncu_dec != '':
-        unlock_json.addr_ncu_dec = unlock_json.addr_ncu_dec - 1
-        addr_ncu = str(unlock_json.addr_ncu_dec.to_bytes(1, byteorder='little'))[4:6]
-    else:
-        addr_ncu = str(unlock_json.addr_ncu_hex)
 
-    if unlock_json.addr_lock_dec != '':
-        unlock_json.addr_lock_dec = unlock_json.addr_lock_dec - 1
-        addr_lock = str(unlock_json.addr_lock_dec.to_bytes(1, byteorder='little'))[4:6]
-    else:
-        addr_lock = str(unlock_json.addr_lock_hex)
+    unlock_json.board = unlock_json.board - 1
+    addr_ncu = str(unlock_json.board.to_bytes(1, byteorder='little'))[4:6]
+    unlock_json.lock = unlock_json.lock - 1
+    addr_lock = str(unlock_json.lock.to_bytes(1, byteorder='little'))[4:6]
 
     addr_ncu =  addr_ncu[1:2]
     addr_lock = addr_lock[1:2]
@@ -244,8 +225,8 @@ async def unlock(unlock_json: Unlock_Json):
     }
 
 #3
-@app.put("/getallstates/")  # '02F0320327'
-async def getallstates():
+@app.put("/getstateall/")  # '02F0320327'
+async def getstateall():
     time_received = datetime.now()
     cmd = '32'
     addr = 'F0'
@@ -268,16 +249,13 @@ async def getallstates():
             }
 
 #4
-@app.put("/openall/")      # Default: '0200330338'
-async def openall(openall_json: Openall_Json):
+@app.put("/unlockall/")      # Default: '0200330338'
+async def unlockall(unlockall_json: UnlockAll_Json):
     time_received = datetime.now()
-    if openall_json.addr_ncu_dec != '':
-        openall_json.addr_ncu_dec = openall_json.addr_ncu_dec - 1
-        addr_ncu = str(openall_json.addr_ncu_dec.to_bytes(2, byteorder='little'))[4:6]
-    else:
-        addr_ncu = str(openall_json.addr_ncu_hex)
+    unlockall_json.board = unlockall_json.board - 1
+    addr_ncu = str(unlockall_json.board.to_bytes(2, byteorder='little'))[5:6]
 
-    addr = addr_ncu
+    addr = addr_ncu + '0' # 00 - board 1, 10 - board 2, 20 - board 3
 
     cmd = '33'
     summa = check_sum2(addr, cmd, '')
@@ -296,9 +274,9 @@ async def openall(openall_json: Openall_Json):
 @app.put("/getunlocktime/")
 async def getunlocktime(getunlocktime_json: Getunlocktime_Json):
     time_received = datetime.now()
-    if getunlocktime_json.addr_ncu_dec != '':
-        getunlocktime_json.addr_ncu_dec = getunlocktime_json.addr_ncu_dec - 1
-        addr_ncu = str(getunlocktime_json.addr_ncu_dec.to_bytes(1, byteorder='little'))[4:6]
+    if getunlocktime_json.board != '':
+        getunlocktime_json.board = getunlocktime_json.board - 1
+        addr_ncu = str(getunlocktime_json.board.to_bytes(1, byteorder='little'))[4:6]
     else:
         addr_ncu = str(getunlocktime_json.addr_ncu_hex)
 
@@ -311,7 +289,7 @@ async def getunlocktime(getunlocktime_json: Getunlocktime_Json):
     response = serial_txrx(command) # Response No by Docs
 
     return {"received_time": time_received,
-            "received_parameters": {"command": "getsetunlocktime", "addr_ncu": addr_ncu},
+            "received_parameters": {"command": "getunlocktime", "addr_ncu": addr_ncu},
             "request_time": time_sent,
             "request_command": command,
             "response": response,
@@ -320,9 +298,9 @@ async def getunlocktime(getunlocktime_json: Getunlocktime_Json):
 @app.put("/setunlocktime/")
 async def setunlocktime(setunlocktime_json: Setunlocktime_Json):
     time_received = datetime.now()
-    if setunlocktime_json.addr_ncu_dec != '':
-        setunlocktime_json.addr_ncu_dec = setunlocktime_json.addr_ncu_dec - 1
-        addr_ncu = str(setunlocktime_json.addr_ncu_dec.to_bytes(1, byteorder='little'))[4:6]
+    if setunlocktime_json.board != '':
+        setunlocktime_json.board = setunlocktime_json.board - 1
+        addr_ncu = str(setunlocktime_json.board.to_bytes(1, byteorder='little'))[4:6]
         data_hex = hex(setunlocktime_json.set_unlocktime_ms_dec)
         data_hex_10 = hex(setunlocktime_json.set_unlocktime_ms_dec // 10)
         data = hex(setunlocktime_json.set_unlocktime_ms_dec // 10)[2:4]
@@ -339,13 +317,36 @@ async def setunlocktime(setunlocktime_json: Setunlocktime_Json):
     response = serial_txrx(command) # Response No by Docs
 
     return {"received_time": time_received,
-            "received_parameters": {"command": "getsetunlocktime", "addr_ncu": addr_ncu},
+            "received_parameters": {"command": "setunlocktime", "addr_ncu": addr_ncu},
             "request_time": time_sent,
             "request_command": command,
             "response": response,
     }
 
 
+@app.put("/getversion/")
+async def getversion(getversion_json: GetVersion_Json):
+    '''
+    ! GetVersion and other OTA functions(0xB5 & 0xB6) NOT work with current old Board CU-V2.1 20201-06-08. Will work after upgrade of Board and Software.
+    '''
+    time_received = datetime.now()
+    getversion_json.addr_board_dec = getversion_json.addr_board_dec - 1
+    addr_board = str(getversion_json.addr_board_dec.to_bytes(1, byteorder='little'))[4:6]
+    cmd = 'B6'
+    command = 'F5' + cmd + addr_board + '005F' + '0A'
+    time_sent = datetime.now()
+    response = serial_txrx(command) # Response No by Docs
+    return {"received_time": time_received,
+            "received_parameters": {#"address": addr,
+                                    "command": "getversion"},
+            "request_time": time_sent,
+            "request_command": command,
+            "response_time": response["response_time"],
+            "response_bytes": response["response_bytes"],
+            "response_hex": response["response_hex"],
+            }
+
+''' ! Also Not Response - May be Unsupported on this old Board/Software
 #8.1
 @app.get("/queryaddresstate/{address}")
 async def queryaddresstate(address: str):
@@ -392,4 +393,46 @@ async def querytime(bus: str):
             print("No data received.")
             return {"response": "None"}
     return {"response": f"{received_data}"}
+'''
 
+# Functions
+def check_sum(request_json: Request_Json):
+    return hex(int(('0x' + request_json.stx), 16)
+                + int(('0x' + request_json.addr), 16)
+                + int(('0x' + request_json.cmd), 16)
+                #                     + int(('0x'+data),16)
+                + int(('0x' + request_json.etx), 16))[-2:]  # last 1(one)[2 symbols] byte from summa
+
+def check_sum2(address: str, command: str, data: str):
+    return hex(int(('0x' + stx), 16)
+                + int(('0x' + address), 16)
+                + int(('0x' + command), 16)
+                #                     + int(('0x'+data),16)
+                + int(('0x' + etx), 16))[-2:]  # last 1(one)[2 symbols] byte from summa
+
+def check_sum3(address: str, command: str, data: str):
+    return hex(int(('0x' + stx), 16)
+                + int(('0x' + address), 16)
+                + int(('0x' + command), 16)
+                + int(('0x' + data),16)
+                + int(('0x' + etx), 16))[-2:]  # last 1(one)[2 symbols] byte from summa
+
+def serial_txrx(send: str):
+    # Send data to the serial device
+    ser.write(bytes.fromhex(send))
+    # Wait a moment for the device to respond
+    time.sleep(0.1)
+    # Read data from the serial device
+    # Read all bytes waiting in the input buffer
+    if ser.in_waiting > 0:
+        # We have to exclude symbol ';' from the end of NCU16 response.
+        received_data = ser.read(ser.in_waiting).replace(b';', b'')
+        print(f"Received: {received_data}")
+    else:
+        received_data = bytes("None Response", 'utf-8')
+        print("No data received.")
+    return {'response_time': datetime.now(),
+            'response_bytes': received_data,
+            'response_hex': received_data.hex() #,
+            #'response_bin': bin(int(received_data, 16))[2:].zfill(len(received_data) * 4)
+            }
